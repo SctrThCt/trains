a72a4716d3fd1fda51bfd056f731689ac0f6e654
Roman Voytov <scooterthecat@yandex.ru>
2023-05-17 21:34:20 +0800

    updated readme

--- src/main/java/stc/trains/util/ValidationUtil.java
+++ src/main/java/stc/trains/util/ValidationUtil.java
@@ -9,12 +9,6 @@ import stc.trains.model.BaseEntity;
 @UtilityClass
 public class  ValidationUtil {
 
-    public static void checkNew(BaseEntity bean) {
-        if (!bean.isNew()) {
-            throw new DataIntegrityViolationException(bean.getClass().getSimpleName() + " must be new (id=null)");
-        }
-    }
-
     //  Conservative when you reply, but accept liberally (http://stackoverflow.com/a/32728226/548473)
     public static void assureIdConsistent(BaseEntity bean, int id) {
         if (bean.isNew()) {
@@ -28,12 +22,5 @@ public class  ValidationUtil {
         if (count == 0) {
             throw new DataIntegrityViolationException("Entity with id=" + id + " not found");
         }
-    }
-
-    //  https://stackoverflow.com/a/65442410/548473
-    @NonNull
-    public static Throwable getRootCause(@NonNull Throwable t) {
-        Throwable rootCause = NestedExceptionUtils.getRootCause(t);
-        return rootCause != null ? rootCause : t;
     }
 }
